c6b59fcb450c424876b1b00bc80fe43ddd7a1ad5
madhurambal1 <rkolanji90@gmail.com>
2026-03-12 16:09:27 +0530

    first commit

--- src/main/java/com/example/demo/repository/ExamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Exam;
-
-public interface ExamRepository extends JpaRepository<Exam, Long> {
-
-}
